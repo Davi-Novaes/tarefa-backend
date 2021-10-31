43605e69c0595c57651037d1915b053d6beb4ee5
Davi-Novaes <davirodriguessk8_@hotmail.com>
2021-10-31 18:48:57 -0300

    Deploy nos Arquivos para Atualizar

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
  
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Servidor em Execução');
 });
 
